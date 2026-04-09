3cc5f05433758ae706d0cfa89edeb8c088f891dd
Fikar <fikar@example.com>
2026-04-09 15:56:43 +0700

    add semua file

---
+++ view/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perkenalan Diri</title>
+</head>
+<body style="font-family:arial;">
+    <!-- Heading -->
+    <h1 >Perkenalan Diri</h1>
+    <h2 >Perkenalan Diri</h2>
+    <h3 >Perkenalan Diri</h3>
+    <h4 >Perkenalan Diri</h4>
+    <h5 >Perkenalan Diri</h5>
+    <hr>
+    <!-- Paragraf -->
+    <p align = "justify" style="font-family:arial;">Saya adalah seorang mahasiswa yang sedang menempuh pendidikan di sebuah universitas.
+    Saya memiliki minat yang besar dalam bidang teknologi dan Artificial Intelligence. Selain itu, saya juga aktif dalam berbagai kegiatan organisasi di kampus.</p>
+    <b>bold teks</b><br>
+    <i>teks miring</i><br>
+    <strong>teks tebal</strong><br>
+    <code>teks kode</code><br>
+    <a href="https://www.instagram.com">Kunjungi Website Saya</a><hr>
+    <ul>
+        <li>Hobi: Membaca buku, bermain game, dan berolahraga.</li>
+        <li>Tujuan: Menjadi seorang ahli di bidang teknologi dan memberikan kontribusi positif bagi masyarakat.</li>
+    </ul>
+    <ol>
+        <li>Nama: Muhammad Zulfikar</li>
+        <li>Usia: 20 tahun</li>
+        <li>Jurusan: Teknik Informatika</li>
+    </ol><hr>
+
+    <img src="https://tse4.mm.bing.net/th/id/OIP.8DnCrbL1lmWDH4VRKQqtegHaHa?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Foto Profil" width="200" height="200" alt="Foto Profil"><hr>
+
+    <table border="1" align="center" width="100%">
+        <tr>
+            <td width="10">
+                <img src="https://tse3.mm.bing.net/th/id/OIP.SmGVJn8qKwHD7J0enkbfZgAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" alt="logo unsiq" width="75" height="75">
+            </td>
+            <td>
+                <h2 width="90">Selamat Datang di Mata Kuliah Pemrograman Web</h2>
+                <h3>Pertemuan 2: HTML Dasar</h3>     
+            </td>
+        </tr>
+    </table>
+    <p align="center">Mata Kuliah ini memperkenalkan dasar-dasar pemrograman web menggunakan HTML,CSS dan JavaScript. Pada Pertemuan ini, kita fokus pada struktur dan elemen dasar HTML</p><hr>
+    <p align = "center">mata kuliah ini mengenalkan dasar dasar pemrograman web HTML, kita bisa menampilkan teks, gambar dan link secara terorganisir. Untuk informasi lengkap dari HTML dapat kunjungi <a href ="https://www.w3schools.com/html/default.asp">link berikut</a></p>
+    <h3>Topik Utama Pertemuan ini</h3>
+    <ul>
+        <li>Struktur HTML</li>
+        <li>Heading dan Paragraf</li>
+        <li>List</li>
+        <li>Link dan Gambar</li>
+        <li>Table</li>
+    </ul><br>
+
+    <h3>Langkah Praktikum</h3>
+    <ol>
+        <li>Buat file index.php</li>
+        <li>Tulis kode HTML</li>
+        <li>Simpan dan buka di browser</li>
+            <ul>
+                <li>Buka xampp</li>
+                <li>Nyalakan Server</li>
+                <li>buka browser dengan url localhost/</li>
+            </ul>
+        <li>Verifikasi hasil</li>    
+</body>
+</html>
